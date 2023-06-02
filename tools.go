26988fb53b2f94aa3dbdfce4418a7746e43f805d
tranthanhnhan <cutin17vn@gmail.com>
2023-06-02 14:09:58 +0700

    upload-file_create-dir-if-not-esixt

--- tools.go
+++ tools.go
@@ -1,7 +1,14 @@
 package toolkit
 
 import (
+	"errors"
+	"fmt"
+	"io"
 	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 	"unicode/utf8"
 )
 
@@ -9,6 +16,8 @@ const randomStringSource = "abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ
 
 // Tools is the type to instantiate this module
 type Tools struct {
+	MaxFileSize      int
+	AllowedFileTypes []string
 }
 
 // RandomString return a string of random characters of length n use randomStringSource
@@ -19,4 +28,186 @@ func (t *Tools) RandomString(n int) string {
 		s[i] = r[rand.Intn(utf8.RuneCountInString(randomStringSource))]
 	}
 	return string(s)
+}
+
+// UploadedFcile is a struct used to save information about an uploaded file
+type UploadFile struct {
+	NewFileName      string
+	OriginalFileName string
+	FileSize         int64
+}
+
+func (t *Tools) UploadOneFile(r *http.Request, uploadDir string, rename ...bool) (*UploadFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+	var uploadedFile UploadFile
+	infile, hdr, err := r.FormFile("file")
+	if err != nil {
+		return nil, err
+	}
+	defer infile.Close()
+
+	err = t.CreateDirIfNotExist(uploadDir)
+	if err != nil {
+		return nil, err
+	}
+
+	buff := make([]byte, 512)
+	_, err = infile.Read(buff)
+	if err != nil {
+		return nil, err
+	}
+
+	// check to see if the file type is permitted
+	allowed := false
+	fileType := http.DetectContentType(buff)
+
+	if len(t.AllowedFileTypes) > 0 {
+		for _, x := range t.AllowedFileTypes {
+			if strings.EqualFold(fileType, x) {
+				allowed = true
+			}
+		}
+	} else {
+		allowed = true
+	}
+
+	if !allowed {
+		return nil, errors.New("the uploaded file type is not permitted")
+	}
+
+	_, err = infile.Seek(0, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	if renameFile {
+		uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
+	} else {
+		uploadedFile.NewFileName = hdr.Filename
+	}
+
+	uploadedFile.OriginalFileName = hdr.Filename
+
+	var outfile *os.File
+	defer outfile.Close()
+
+	if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+		return nil, err
+	} else {
+		fileSize, err := io.Copy(outfile, infile)
+		if err != nil {
+			return nil, err
+		}
+		uploadedFile.FileSize = fileSize
+	}
+
+	return &uploadedFile, nil
+}
+
+func (t *Tools) UploadFiles(r *http.Request, uploadDir string, rename ...bool) ([]*UploadFile, error) {
+	renameFile := true
+	if len(rename) > 0 {
+		renameFile = rename[0]
+	}
+
+	var uploadedFiles []*UploadFile
+	if t.MaxFileSize == 0 {
+		t.MaxFileSize = 1024 * 1024 * 1024
+	}
+
+	err := t.CreateDirIfNotExist(uploadDir)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.ParseMultipartForm(int64(t.MaxFileSize))
+
+	if err != nil {
+		return nil, errors.New("the uploaded file is too big")
+	}
+
+	for _, fHeaders := range r.MultipartForm.File {
+		for _, hdr := range fHeaders {
+			uploadedFiles, err = func(uploadedFiles []*UploadFile) ([]*UploadFile, error) {
+				var uploadedFile UploadFile
+				infile, err := hdr.Open()
+				if err != nil {
+					return nil, err
+				}
+				defer infile.Close()
+
+				buff := make([]byte, 512)
+				_, err = infile.Read(buff)
+				if err != nil {
+					return nil, err
+				}
+
+				// check to see if the file type is permitted
+				allowed := false
+				fileType := http.DetectContentType(buff)
+
+				if len(t.AllowedFileTypes) > 0 {
+					for _, x := range t.AllowedFileTypes {
+						if strings.EqualFold(fileType, x) {
+							allowed = true
+						}
+					}
+				} else {
+					allowed = true
+				}
+
+				if !allowed {
+					return nil, errors.New("the uploaded file type is not permitted")
+				}
+
+				_, err = infile.Seek(0, 0)
+				if err != nil {
+					return nil, err
+				}
+
+				if renameFile {
+					uploadedFile.NewFileName = fmt.Sprintf("%s%s", t.RandomString(25), filepath.Ext(hdr.Filename))
+				} else {
+					uploadedFile.NewFileName = hdr.Filename
+				}
+
+				uploadedFile.OriginalFileName = hdr.Filename
+
+				var outfile *os.File
+				defer outfile.Close()
+
+				if outfile, err = os.Create(filepath.Join(uploadDir, uploadedFile.NewFileName)); err != nil {
+					return nil, err
+				} else {
+					fileSize, err := io.Copy(outfile, infile)
+					if err != nil {
+						return nil, err
+					}
+					uploadedFile.FileSize = fileSize
+				}
+
+				uploadedFiles = append(uploadedFiles, &uploadedFile)
+				return uploadedFiles, nil
+			}(uploadedFiles)
+			if err != nil {
+				return uploadedFiles, err
+			}
+		}
+	}
+
+	return uploadedFiles, nil
+}
+
+func (t *Tools) CreateDirIfNotExist(path string) error {
+	const mode = 0755
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.MkdirAll(path, mode)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- tools_test.go
+++ tools_test.go
@@ -1,6 +1,8 @@
 package toolkit
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 	"unicode/utf8"
 )
@@ -11,4 +13,21 @@ func TestTools_RandomString(t *testing.T) {
 	if utf8.RuneCountInString(s) != 10 {
 		t.Error("wrong length")
 	}
+}
+
+func TestTool_CreateDirIfNotExist(t *testing.T) {
+	var testTool Tools
+	absPath, _ := filepath.Abs("./myDir")
+
+	err := testTool.CreateDirIfNotExist(absPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testTool.CreateDirIfNotExist(absPath)
+	if err != nil {
+		t.Error(err)
+	}
+
+	_ = os.Remove(absPath)
 }
